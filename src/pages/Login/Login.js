98f01ea6019bb15af61f30ea85813690256c4011
frausafiye <s_gulenc@outlook.com>
2024-03-20 11:11:28 +0100

    configured after merging

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -26,7 +26,6 @@ export default function Login(props) {
     axios({
       method: "POST",
       withCredentials: true,
-      //sameSite: "none",
       url: `${process.env.REACT_APP_BASE_URL}/users/login`,
       headers: {
         Accept: "application/json",
